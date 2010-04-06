dbcfd73012562d791f5e8f2070d052555fa7cf8d
keul <luca@keul.it>
2010-04-06 14:29:51 +0000

    For me this is a final 0.1.0 version: all featues are there. jQuery.live() removed.

--- README.txt
+++ README.txt
@@ -17,7 +17,7 @@ navigation elements. Sometimes your site structure is done like this::
 
     ROOT
     |
-    |_ Folder
+    |_ FolderWhatever
     \_ NotImportantFolder
        |
        |_ ImportantFolder1
@@ -27,9 +27,19 @@ navigation elements. Sometimes your site structure is done like this::
 If you configure your navigation portlet on the site root, users must click on the *NotImportantFolder*
 and reload the whole page to reach the *ImportantFolder*'s section.
 The site at the *NotImportantFolder* level is not useful... maybe you only give a *folder_listing* view
-or a default page that only say "Welcome to an important area of the site, please visit... bla bla bla".
+or a default page that only say "Welcome to an important area of the site, please visit the subsection
+you are looking for...".
 
 For the user experience the first click is only a waste of time.
+What you are looking for can be a client side effect that expand/collapse the folder in the navigation.
+The *NotImportantFolder* itself is not seen as a real content.
+
+What will change
+----------------
+
+Some special navigation links will no more move the user the the target section but simply shows in the
+navigation itself all subsections. A second click will close this.
+Default page in a folder and elements marked with "Exclude from navigation" are skipped.
 
 How to use
 ----------
@@ -37,7 +47,63 @@ How to use
 Right now this is a product *targeted on developers*. You *must* provide a very simple Javascript
 script to use and configure it.
 
+You must add additional Javascript source(s) like this::
 
+    jQuery.collective_navigationtoggle['toggle_elements'].push("/foo1/foo2");
+
+Where "*/foo1/foo2*" can be an existing suffix of an *href* attribute for a link. Only link inside
+navigation portlet are checked (looking for "*portletNavigationTree*" class).
+
+So, a link like this (*if* inside a navigation portlet) is "hit" and macically handled::
+
+    <a href="http://plonehost/foo/foo1/foo2">
+
+This because the *href* ends with one of the elements found inside *toggle_elements*.
+
+Another possible configuration::
+
+    jQuery.collective_navigationtoggle['toggle_elements'].push("/foo1/foo2");
+    jQuery.collective_navigationtoggle['toggle_elements'].push("/foo1/foo2/foo3");
+
+This time the "*foo3*" folder is inside the "*foo2*" and can be possible that the last link is not available
+at load page time (because for exampe we are still in the Plone root). However collective.navigationtoggle
+perform the binding of expand/collapse action also for not-yet-loaded elements. 
+
+Please, do not include the "/plonesiteid" part in your path or you will have problems when you put
+Apache in front of Zope.
+
+Whatever configuration you wrote, you **must** include you Javascript(s) file inside *portal_javascript*
+tool *after* the *collective.navigationtoggle.js*::
+
+    <javascript cacheable="True"
+             compression="safe"
+             cookable="True"
+             enabled="True"
+             id="my-configuration-javascript-load-path.js"
+             insert-after="++resource++collective.navigationtoggle.js"
+             inline="False" />
+
+Styles
+------
+
+This products dinamically adds two new possible CSS classes to <li> elements that contains links that match
+the configuration.
+
+Class "*cnavClosed*" is added when a special navigation elements is shown on the page and when you close
+a subtree.
+When a subsection is open, the class "*cnavOpen*" is added to the same element.
+
+You can (but this product will don't) rely on those classes to give additional styles effects in your
+Plone theme.
+
+This products *may* also works on non-standard navigation HTML structure (as the generations on subelement
+is done cloning existings node from the same navigation portlet) but some assumptions are done. 
+
+TODO
+====
+
+* Cache already queryed subtrees (to prevent useless AJAX call if you continue to click on the same element)
+* Give a Plone (or at least ZMI) UI for non-developer users, for TTW configuration.
 
 Other products
 ==============
--- collective/navigationtoggle/browser/collective.navigationtoggle.js
+++ collective/navigationtoggle/browser/collective.navigationtoggle.js
@@ -1,5 +1,8 @@
 /**
- * The collective.navigationtoggle Javascript source
+ * The collective.navigationtoggle Javascript source.
+ * Before think "Ehi, I can write this code more and more jQueryish" be aware...
+ * Here I'm trying to get the same feature given up by the jQuery.live() method whitout
+ * using it (due to some limitation I found).
  */
 
 jQuery.collective_navigationtoggle = {
@@ -15,8 +18,19 @@ jQuery.collective_navigationtoggle = {
 
 jq(document).ready(function() {
 
+	/**
+	 * Context URL to be used for all AJAX call
+	 */
 	var call_context = jq("head base").attr('href');
-	if (!call_context.charAt(call_context.length-1)!='/') call_context=call_context+'/';
+	if (call_context.charAt(call_context.length-1)!='/') call_context=call_context+'/';
+
+	/*
+	 * Don't want to call the context when is in the portal factory. See the Ale's blog post:
+	 * http://blog.redturtle.it/redturtle-blog/2010/03/11/careful-with-that-ajax-eugene
+	 */
+	if (call_context.indexOf('/portal_factory')>-1) {
+		call_context=call_context.substring(0,call_context.indexOf('/portal_factory')+1);
+	}
 
 	/**
 	 * Generate a new navigation element
@@ -47,39 +61,68 @@ jq(document).ready(function() {
 		}
 	}
 	
-	jq.each(jq.collective_navigationtoggle.toggle_elements, function(index, value) {
+	/**
+	 * A function to be called with jQuery.each(). Check all element defined in the
+	 * toggle_elements, then apply to them the "toggle" feature.
+	 * @param {Object} index iteration index
+	 * @param {Object} value current element
+	 */
+	var makeDynamicElements = function(index, value) {
 		var control = jq(".portletNavigationTree a[href$="+value+"]");
+		if (control.length==0) return;
 		var main_elem = control.parents("li:first");
+		if (main_elem.hasClass('cnavClosed') || main_elem.hasClass('cnavOpen')) return;
 		var ul_model = main_elem.parents('ul:first').clone().addClass('cnavGenerated').empty();
 		var li_model = main_elem.clone().empty();
 		// Check the right CSS class to be given to the main element
-		if (main_elem.children(":last").is("ul"))
-			main_elem.addClass('cnavOpen');
+		if (main_elem.children(":last").is("ul")) main_elem.addClass('cnavOpen');
 		else main_elem.addClass('cnavClosed');
-		control.click(function(event) {
-			if (main_elem.hasClass('cnavClosed')) {
-				new_ul = ul_model.clone();
-				main_elem.append(new_ul).removeClass('cnavClosed').addClass('cnavOpen');
-				riseNavigatorClass(new_ul);
-				// Fill the new element
-				jq.getJSON(call_context+'@@query-subelements',
-						{'path': jq('a', main_elem).attr('href')},
-						function(data) {
-							jq.each(data, function(index, value) {
-								new_ul.append(makeSubelement(value, li_model.clone()));
-							});
-						}
-				);
-				event.preventDefault();
-			}
-			else if (main_elem.hasClass('cnavOpen')) {
-				main_elem.children(":last").remove().end()
-						.removeClass('cnavOpen').addClass('cnavClosed');
-				event.preventDefault();
-			};
-		})
-	});
+		control.bind("click", {main_elem: main_elem, ul_model:ul_model, li_model:li_model},
+				checkClick);
+	};
+	
+	/**
+	 * Click handler to be used on expasible/collapsible navigation elements.
+	 * @param {Event} event jQuery event object, containing the data structure
+	 */
+	var checkClick = function(event) {
+		main_elem = event.data.main_elem;
+		ul_model = event.data.ul_model;
+		li_model = event.data.li_model;
+		if (main_elem.hasClass('cnavClosed')) {
+			new_ul = ul_model.clone();
+			main_elem.append(new_ul).removeClass('cnavClosed').addClass('cnavOpen');
+			riseNavigatorClass(new_ul);
+			// Fill the new element
+			jq.getJSON(call_context+'@@query-subelements',
+					{'path': jq('a', main_elem).attr('href')},
+					function(data) {
+						jq.each(data, function(index, value) {
+							new_ul.append(makeSubelement(value, li_model.clone()));
+						});
+						// Now we simulate here the jQuery.live feature...
+						checkDOM();
+					}
+			);
+			event.preventDefault();
+		}
+		else if (main_elem.hasClass('cnavOpen')) {
+			main_elem.children(":last").remove().end()
+					.removeClass('cnavOpen').addClass('cnavClosed');
+			event.preventDefault();
+		};
+	};
+	
+	/**
+	 * Simple function that perform the whole check for every possible navigation elements
+	 * that needs toggling feature.
+	 */
+	var checkDOM = function() {
+		jq.each(jq.collective_navigationtoggle.toggle_elements, makeDynamicElements);
+	};
+	checkDOM();
+	
 })
 
-jQuery.collective_navigationtoggle['toggle_elements'].push("/test/folder-a");
-jQuery.collective_navigationtoggle['toggle_elements'].push("/test/folder-a/folder-a1");
+// jQuery.collective_navigationtoggle['toggle_elements'].push("/folder-a");
+// jQuery.collective_navigationtoggle['toggle_elements'].push("/test/folder-a/folder-a1");
