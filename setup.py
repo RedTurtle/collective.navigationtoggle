cd87cbc4f61c7fb7760e5e03379106ed18d1985b
Luca Fabbri <luca@keul.it>
2012-11-13 10:43:24 +0100

    moved documentation to rest

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(name='collective.navigationtoggle',
       version=version,
       description=("Expand/collapse feature for specific Plone "
                    "navigation entries, in an unobtrusive way"),
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -32,7 +32,7 @@ setup(name='collective.navigationtoggle',
         ],
       keywords='plone plonegov jquery navigation javascript',
       author='RedTurtle Technology',
-      author_email='sviluppoplone@redturtle.net',
+      author_email='sviluppoplone@redturtle.it',
       url='http://plone.org/products/collective.navigationtoggle',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
