f660df31e4d935aa05259c5bf8338c6f366928c5
Luca Fabbri <luca@keul.it>
2012-11-15 17:37:21 +0100

    basic JavaScript tests with Jasmine

---
+++ collective/navigationtoggle/tests/javascript/spec/AJAXSpec.js
@@ -0,0 +1,111 @@
+describe("AJAX related toggle navigation features", function() {
+
+	var test1_navigation, count_call;
+
+	beforeEach(function() {
+		test1_navigation = $('#test1 dl').clone();
+		count_call = 0;
+		$.collective_navigationtoggle.toggle_elements = [];
+		$.collective_navigationtoggle.cache = true;
+		spyOn($, "getJSON").andCallFake(function(query, params, callback) {
+			count_call++;
+			if (params['path']=='/Plone/test') {
+				var data = [{"review_state_normalized": "published", "description": "", "type_normalized": "document", "url": "/Plone/test/pagina-2", "title": "Pagina 2", "type": "Document", "icon": "/Plone/"},
+				            {"review_state_normalized": "published", "description": "", "type_normalized": "folder", "url": "/Plone/test/subtest", "title": "Subtest", "type": "Folder", "icon": "/Plone/"}];
+				callback(data);
+			} else if (params['path']=='/Plone/test/subtest') {
+				var data = [{"review_state_normalized": "published", "description": "", "type_normalized": "link", "url": "/Plone/test/subtest/repubblica", "title": "Repubblica", "type": "Link", "icon": "/Plone/"}];
+				callback(data);
+			};
+
+		});
+	});
+	
+	afterEach(function() {
+		$('#test1').empty().append(test1_navigation);	
+	});
+
+	it("should populate with sub objects", function() {
+		$.collective_navigationtoggle.toggle_elements.push('/test');
+		$(document).trigger('checkDOM');
+		$('#test1 .navTree .cnavClosed a').click();
+		expect($('#test1 .navTree .cnavOpen .navTreeLevel1').length).toEqual(1);
+		var subelements = $('#test1 .navTree .cnavOpen .navTreeLevel1 a');
+		expect($(subelements[0]).attr('href')).toEqual('/Plone/test/pagina-2');
+		expect($(subelements[1]).attr('href')).toEqual('/Plone/test/subtest');
+	});
+
+	it("should manage caching of sub objects", function() {
+		$.collective_navigationtoggle.toggle_elements.push('/test');
+		$(document).trigger('checkDOM');
+		// open
+		$('#test1 .navTree .cnavClosed a').click();
+		// close
+		$('#test1 .navTree .cnavOpen a').click();
+		// re-open
+		$('#test1 .navTree .cnavClosed a').click();
+		var subelements = $('#test1 .navTree .cnavOpen .navTreeLevel1 a');
+		expect($(subelements[0]).attr('href')).toEqual('/Plone/test/pagina-2');
+		expect($(subelements[1]).attr('href')).toEqual('/Plone/test/subtest');
+		expect(count_call).toBe(1);
+		// let's disable cache
+		$.collective_navigationtoggle.cache = false;
+		count_call = 0;
+		// close
+		$('#test1 .navTree .cnavOpen a').click();
+		// re-open
+		$('#test1 .navTree .cnavClosed a').click();
+		expect(count_call).toBe(1);
+	});
+
+	it("should populate sub objects and oobjects inside suobjects", function() {
+		$.collective_navigationtoggle.toggle_elements.push('/test');
+		$.collective_navigationtoggle.toggle_elements.push('/test/subtest');
+		$(document).trigger('checkDOM');
+		$('#test1 .navTree .cnavClosed a').click();
+		$('#test1 .navTree .cnavOpen .navTreeLevel1 a').click();
+		expect(count_call).toBe(2);
+		var subelements = $('#test1 .navTree .cnavOpen .navTreeLevel2 a');
+		expect(subelements.length).toBe(1);
+		expect(subelements.attr('href')).toEqual('/Plone/test/subtest/repubblica');
+	});
+
+	it("should manage caching of both sub objects and childs", function() {
+		$.collective_navigationtoggle.toggle_elements.push('/test');
+		$.collective_navigationtoggle.toggle_elements.push('/test/subtest');
+		$(document).trigger('checkDOM');
+		// open
+		$('#test1 .navTree .cnavClosed a').click();
+		// close
+		$('#test1 .navTree .cnavOpen a').click();
+		// re-open
+		$('#test1 .navTree .cnavClosed a').click();
+		// open sub-section
+		$('#test1 .navTree .cnavOpen .navTreeLevel1 a').click();
+		// close sub-section
+		$('#test1 .navTree .cnavOpen .navTreeLevel1 .cnavOpen a').click();
+		// re-open sub-section
+		$('#test1 .navTree .cnavOpen .navTreeLevel1 a').click();
+		expect(count_call).toBe(2);		
+		// let's disable cache
+		$.collective_navigationtoggle.cache = false;
+		count_call = 0;
+		// close all
+		$('#test1 .navTree .cnavOpen a').click();
+		// open
+		$('#test1 .navTree .cnavClosed a').click();
+		// close
+		$('#test1 .navTree .cnavOpen a').click();
+		// re-open
+		$('#test1 .navTree .cnavClosed a').click();
+		// open sub-section
+		$('#test1 .navTree .cnavOpen .navTreeLevel1 a').click();
+		// close sub-section
+		$('#test1 .navTree .cnavOpen .navTreeLevel1 .cnavOpen a').click();
+		// re-open sub-section
+		$('#test1 .navTree .cnavOpen .navTreeLevel1 a').click();
+		expect(count_call).toBe(4);
+	});
+
+
+});
--- collective/navigationtoggle/tests/javascript/spec/BasicSpec.js
+++ collective/navigationtoggle/tests/javascript/spec/BasicSpec.js
@@ -1,27 +1,44 @@
 describe("Basic toggle navigation features", function() {
 
-  it("should set the cnavClosed CSS class", function() {
-  	$.collective_navigationtoggle.toggle_elements.push('/test')
-    $(document).trigger('checkDOM');
-	expect($.trim($('#test1 .navTree .cnavClosed').text())).toBe('Test');
-  });
+	var test1_navigation;
 
-  it("should set the cnavOpen CSS class", function() {
-  	spyOn($, "getJSON");
-  	$.collective_navigationtoggle.toggle_elements.push('/test')
-	$(document).trigger('checkDOM');
-	var link = $('#test1 .navTree .cnavClosed a');
-	link.click();
-	expect($.trim($('#test1 .navTree .cnavOpen').text())).toBe('Test');
-  });
+	beforeEach(function() {
+		test1_navigation = $('#test1 dl').clone();
+		$.collective_navigationtoggle.toggle_elements = [];
+	});
+	
+	afterEach(function() {
+		$('#test1').empty().append(test1_navigation);		
+	});
 
-  it("should pass parameters related to /test folder", function() {
-  	spyOn($, "getJSON");
-  	$.collective_navigationtoggle.toggle_elements.push('/test')
-	$(document).trigger('checkDOM');
-	var link = $('#test1 .navTree .cnavClosed a');
-	link.click();
-	expect($.getJSON.mostRecentCall).toEqual("/products/123");
-  });
+	it("should set the cnavClosed CSS class", function() {
+		$.collective_navigationtoggle.toggle_elements.push('/test')
+		$(document).trigger('checkDOM');
+		expect($.trim($('#test1 .navTree .cnavClosed').text())).toBe('Test');
+	});
+
+	it("should set the cnavOpen CSS class", function() {
+		spyOn($, "getJSON");
+		$.collective_navigationtoggle.toggle_elements.push('/test')
+		$(document).trigger('checkDOM');
+		$('#test1 .navTree .cnavClosed a').click();
+		expect($.trim($('#test1 .navTree .cnavOpen').text())).toBe('Test');
+	});
+
+	it("should call @@query-subelements view", function() {
+		spyOn($, "getJSON");
+		$.collective_navigationtoggle.toggle_elements.push('/test')
+		$(document).trigger('checkDOM');
+		$('#test1 .navTree .cnavClosed a').click();
+		expect($.getJSON.mostRecentCall.args[0]).toEqual("/@@query-subelements");
+	});
+
+	it("should pass parameters related to /test folder", function() {
+		spyOn($, "getJSON");
+		$.collective_navigationtoggle.toggle_elements.push('/test')
+		$(document).trigger('checkDOM');
+		$('#test1 .navTree .cnavClosed a').click();
+		expect($.getJSON.mostRecentCall.args[1]['path']).toEqual("/Plone/test");
+	});
 
 });
